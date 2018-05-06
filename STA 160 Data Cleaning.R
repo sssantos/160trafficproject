d1bbb15c4ae6e74e08a6f62613a4c888a8e4cce7
Evan Schulze <evanschulze@Evans-MacBook-Pro-2.local>
2018-05-06 13:20:58 -0700

    Reformatted time once again, also began working on cumulative plots

--- STA 160 Data Cleaning.R
+++ STA 160 Data Cleaning.R
@@ -2,15 +2,28 @@ df<- read.csv("/Users/evanschulze/Desktop/d03_text_station_raw_2018_04_25.txt", 
 df$V2<-as.factor(df$V2) 
 df<-df[df$V2 %in% c("314025","318076","318067","318113","316803"),] ##only keep sensors where bottlenecks exist
 
-date <- sapply(df$V1, function(x) gsub(" \\d{2}:\\d{2}:\\d{2}", "", as.character(x)))
-time <- sapply(df$V1, function(x) gsub("\\d{2}/\\d{2}/\\d{4} ", "", as.character(x)))
-df <- data.frame(date, time, df[-1])
-date <- as.Date(date, "%m/%d/%y")
-time <- chron(times=time, format=c(times="h:m:s"))
+df$Date <- as.POSIXct(df$V1, format='%d/%m/%Y %H:%M:%S')
 
+##subset df into 3PM-8PM only
+df <- setDT(df)[,time:=as.ITime(time)][time>=as.ITime('15:00:00') & time<=as.ITime('20:00:00')]
 
+cars1=df$V3 ##isolate vehicle counts from df
+cars2=df$V6
+cars3=df$V9
+carsdf=data.frame(cars1,cars2,cars3)
+df$sums=rowSums(carsdf) ##find sum for all 3 lanes by time
 
-class(df$time)
+##create cumulative frequency plot according to time
+ggplot(data = df, aes(x = time, y = cumsum(sums))) + geom_line() + 
+  geom_point() + theme(axis.text.x = element_text(angle=90, hjust = 1)) + 
+  scale_x_discrete(labels = df$time) + xlab("time")
 
-strptime(time[1],"%H:%M:%S")
-?chron
+occ1=df$V4 ##isolate occupancy percentages from df
+occ2=df$V7
+occ3=df$V10
+occdf=data.frame(occ1,occ2,occ3)
+df$occ_sums=rowSums(occdf) ##find sum for all 3 lanes
+ggplot(data = df, aes(x = time, y = cumsum(occ_sums))) + geom_line() + 
+  geom_point() + theme(axis.text.x = element_text(angle=90, hjust = 1)) + 
+  scale_x_discrete(labels = df$time) + xlab("time")
+
