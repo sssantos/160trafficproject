46c8bae90e706c62eadd366e92659533d445a761
Evan Schulze <evanschulze@campus-032-072.ucdavis.edu>
2018-05-12 16:35:05 -0700

    Finished up oblique plots and now am beginning to work on double Y scale plot to display queued and unqueued conditions for bottlenecks at each station.

--- STA 160 Data Cleaning.R
+++ STA 160 Data Cleaning.R
@@ -28,15 +28,23 @@ cars2=df.1$V6
 cars3=df.1$V9
 carsdf=data.frame(cars1,cars2,cars3)
 df.1$sums=rowSums(carsdf) ##find sum for all 3 lanes by time
-sum(df.1$sums)
+sum(df.1$occ_sums)
 
 strt<-as.POSIXct('2018-04-25 15:00:00')
 end<-as.POSIXct('2018-04-25 20:00:00')
 ##create cumulative frequency plot according to time
 ggplot(df.1, aes(x=V1, y=cumsum(sums))) + geom_line() + geom_segment(aes(x=strt,y=0,xend=end,yend=sum(sums)), lty="dashed")+
-  theme_bw() + xlab("Time") + ylab("Activity (Vehicle Counts per 30 seconds)") +
-  scale_x_datetime(breaks=seq(strt,end,"30 min"), labels = strftime(seq(strt,end,"30 min"), "%H:%M"))
+  theme_bw() + xlab("Time") + ylab("Cumulative Vehicle Counts per 30 seconds") +
+  scale_x_datetime(breaks=seq(strt,end,"30 min"), labels = strftime(seq(strt,end,"30 min"), "%H:%M")) + 
+  ggtitle('Number of Vehicles Driving Through Station 314025') + theme(plot.title = element_text(hjust = 0.5))
 
+df.1$vph_mean <- rep(32.07, length(df.1$V1))
+df.1$vph_diff <- df.1$sums-df.1$vph_mean ##calculate residulas
+##oblique plot using flow residulas
+ggplot(df.1, aes(x=V1, y=cumsum(vph_diff))) + geom_line() + 
+  theme_bw() + xlab("Time") + ylab("Cumulative Vehicle Counts per 30 seconds") +
+  scale_x_datetime(breaks=seq(strt,end,"30 min"), labels = strftime(seq(strt,end,"30 min"), "%H:%M")) + 
+  ggtitle('Number of Vehicles Driving Through Station 314025') + theme(plot.title = element_text(hjust = 0.5))
 
 occ1=df.1$V4 ##isolate occupancy percentages from df
 occ2=df.1$V7
@@ -44,6 +52,29 @@ occ3=df.1$V10
 occdf=data.frame(occ1,occ2,occ3)
 df.1$occ_sums=rowSums(occdf) ##find sum for all 3 lanes
 ggplot(df.1, aes(x=V1, y=cumsum(occ_sums))) + geom_line() + geom_segment(aes(x=strt,y=0,xend=end,yend=sum(occ_sums)), lty="dashed")+
+<<<<<<< HEAD
+  theme_bw() + xlab("Time") + ylab("Lane Occupancy Percentage per 30 seconds") +
+  scale_x_datetime(breaks=seq(strt,end,"30 min"), labels = strftime(seq(strt,end,"30 min"), "%H:%M")) +
+  ggtitle('Lane Occupancy Percentage Through Station 314025') + theme(plot.title = element_text(hjust = 0.5))
+
+df.1$occ_mean <- rep(.837, length(df.1$V1)) 
+df.1$occ_diff <- df.1$occ_mean-df.1$occ_sums ##calculate residuals
+##oblique plot using occupancy residuals
+ggplot(df.1, aes(x=V1, y=cumsum(occ_diff))) + geom_line() + 
+  theme_bw() + xlab("Time") + ylab("Lane Occupancy Percentage per 30 seconds") +
+  scale_x_datetime(breaks=seq(strt,end,"30 min"), labels = strftime(seq(strt,end,"30 min"), "%H:%M")) +
+  ggtitle('Lane Occupancy Percentage Through Station 314025') + theme(plot.title = element_text(hjust = 0.5))
+
+##scatterplot of occupancy vs. flow
+ggplot(df.1, aes(x=occ_sums, y=sums))+geom_point(shape=1)
+
+##attempt at two Y scales... work in progress
+library(latticeExtra)
+obj1 <- xyplot(occ_sums ~ V1, df.1, type = "l" , lwd=2)
+obj2 <- xyplot(sums ~ V1, df.1, type = "l", lwd=2)
+doubleYScale(obj1, obj2, add.ylab2 = TRUE)
+=======
   theme_bw() + xlab("Time") + ylab("Activity (lane Occupancy Percentage per 30 seconds)") +
   scale_x_datetime(breaks=seq(strt,end,"30 min"), labels = strftime(seq(strt,end,"30 min"), "%H:%M"))
 
+>>>>>>> 27c4c93b5081bd3757d88db74bd7e7c2b367d349
